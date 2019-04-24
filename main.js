8b993825ebbe9a9797b52e1f39c9d4856107b69f
Qiudingding <272312267@qq.com>
2019-04-24 21:18:41 +0800

    上传无缝轮播文件

---
+++ main.js
@@ -0,0 +1,62 @@
+let n
+初始化()
+let timer = setInterval(function() { 
+  makeLeave(getImage(n))
+            .one('transitionend', function(e){
+      makeEnter($(e.currentTarget))
+      })  
+  makeCurrent(getImage(n+1))
+  n += 1
+}, 2000)
+
+document.addEventListener('visibilitychange', function(e){
+  if(document.hidden){
+      window.clearInterval(timer)
+  }else{
+      timer = setInterval(function() { 
+        makeLeave(getImage(n))
+            .one('transitionend', function(e){
+              makeEnter($(e.currentTarget))
+          })  
+        makeCurrent(getImage(n+1))
+        n += 1
+      }, 2000)
+  }
+})
+
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+function x(n){
+    if(n>6){
+      n = n%6
+    if (n === 0){
+      n = 6
+    }
+  } // n = 1 2 3 4 5 6
+  return n
+}
+
+function 初始化(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+}
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+ return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('current leave').addClass('enter')
+}
+
+
+
+
+
+
+
+
+
+
